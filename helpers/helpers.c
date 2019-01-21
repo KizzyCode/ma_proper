a6088dc9587055664b6349e78403e4e1ecfeaca0
Keziah Biermann <development@kizzycode.de>
2019-01-21 01:11:41 +0100

    Improved warnings and errors

--- helpers/helpers.c
+++ helpers/helpers.c
@@ -53,10 +53,10 @@ void ma_proper_memzero(uint8_t* const ptr, const size_t len) {
 	#elif defined(USE_EXPLICIT_MEMSET)
 		explicit_memset(ptr, 0, len);
 	#elif defined(USE_VOLATILE_POINTERS)
-		#warning "No secure `memset` alternative known; using volatile poiners"
+		#warning "No secure memset implementation known; using volatile poiners"
 		volatile uint8_t* volatile ptr_ = (volatile uint8_t* volatile)ptr;
 		for (size_t i = 0; i < len; i++) ptr_[i] = 0x00;
 	#else
-		#error "No secure memset implementation specified"
+		#error "No secure memset implementation specified (this is a build-system error)"
 	#endif
 }
